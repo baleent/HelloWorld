5cbfb842d663bae1002ec8021e80a36eea3dfa72
Balint Bako <bbalintv@freemail.hu>
2011-04-24 23:50:55 +0200

    final

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 HelloWorld::Application.routes.draw do
+  resources :posts do  
+    resources :comments
+  end
+
+  root :to => "home#index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
